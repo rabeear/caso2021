78263893b0a77d42e5cb0bc04edfba6676dd740d
rabeear <rabearupv@gmail.com>
2021-04-08 18:18:23 +0200

    Versión 1.0.2 Cambios: Nuevas ventanas pero faltan por acabar

---
+++ src/javafxconnect4/VistaCambiarAvatarController.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafxconnect4;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author belen
+ */
+public class VistaCambiarAvatarController implements Initializable {
+
+    private Stage stageAvatar;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    void initStage(Stage stage) {
+        stageAvatar = stage;    
+    }
+    
+}
