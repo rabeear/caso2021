c42479019c4078626eb678b980ded7c3566b3d9c
rabeear <rabearupv@gmail.com>
2021-05-20 10:16:11 +0200

    2.5.2.1 cambiarImg + css

---
+++ src/javafxconnect4/VistaCambiarController.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafxconnect4;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author belen
+ */
+public class VistaCambiarController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void clickImg1(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickImg2(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickImg3(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickImg4(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickImg5(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickCancelar(ActionEvent event) {
+    }
+    
+}
